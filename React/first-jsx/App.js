249988b7f60fe51e9f3082540418cebb6d74687c
akawoa <72364753+akawoa@users.noreply.github.com>
2021-08-31 14:16:43 -0700

    Add files via upload

---
+++ React/first-jsx/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+<h1 class="my-class">Hello Dojo</h1>  
+<h2>Things I need to do:</h2>
+<ul>
+  <li>Learn JSX and React</li>
+  <li>Learn to connect App.js to logic</li>
+  <li>Learn routing within express and react</li>
+  <li>Have fun!</li>
+</ul>
+    </div>
+    
+  );
+}
+
+export default App;
