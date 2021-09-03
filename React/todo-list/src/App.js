0b5e859060cebbf8e58b225e4243173b73f5b745
akawoa <72364753+akawoa@users.noreply.github.com>
2021-09-03 14:01:03 -0700

    added assignment

---
+++ React/todo-list/src/App.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import './App.css';
+
+import Todo from "./components/Todo";
+
+
+function App() {
+
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+
+    if (newTodo.length == 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false
+    }
+
+    //setTodos and pass in a brand new array containing all current todos plus 1 more.
+    setTodos([...todos, todoItem]);
+    setNewTodo("");
+  }
+
+  const handleTodoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((todo, i) => {
+      return i != delIdx;
+    });
+
+    setTodos(filteredTodos);
+  }
+
+  const handleToggleComplete = (idx) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if (idx == i) {
+        todo.complete = !todo.complete;
+        //to avoid mutating the todo directly do this
+        // const updatedTodo = {...todo, complete: !todo.complete};
+        // return updatedTodo;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  return (
+    <div>
+      <form onSubmit={(event) => { handleNewTodoSubmit(event) }}>
+        <input onChange={(event) => {
+          setNewTodo(event.target.value);
+        }} type="text"
+          value={newTodo} />
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+
+      {
+        todos.map((todo, i) => {
+          return (
+          <Todo 
+          key={i}
+          i={i}
+          todo={todo} 
+          handleToggleComplete={handleToggleComplete}handleTodoDelete={handleTodoDelete} />
+          )
+        })}
+
+
+
+    </div>
+  );
+}
+
+export default App;
